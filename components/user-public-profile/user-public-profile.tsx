824a3a426e253d2b93138d5ecb1e7d1b919387e9
Vronx93 <92084864+Vronx93@users.noreply.github.com>
2024-07-18 04:01:23 +0200

    added contact info to public profile

--- components/user-public-profile/user-public-profile.tsx
+++ components/user-public-profile/user-public-profile.tsx
@@ -5,12 +5,16 @@ import styles from "./user-public-profile.module.css";
 
 interface UserPublicProfileInterface {
   username: string;
+  email?: string;
+  phone?: string;
   about?: string;
   listedApartments?: ApartmentListElementProps[];
 }
 
 export default function UserPublicProfile({
   username,
+  email,
+  phone,
   about,
   listedApartments,
 }: UserPublicProfileInterface) {
@@ -21,6 +25,16 @@ export default function UserPublicProfile({
           <h1>{username}</h1>
         </header>
         <p>{about && about}</p>
+        {email && (
+          <span>
+            Email: <p>{email}</p>
+          </span>
+        )}
+        {phone && (
+          <span>
+            Phone: <p>{phone}</p>
+          </span>
+        )}
       </article>
       {listedApartments && <h2>Listed Apartments</h2>}
       {listedApartments && (
